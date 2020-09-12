7902c8c7d508fdce44938d60f402adec62907b46
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2020-09-12 23:43:17 +0100

    controllers

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MainController extends Controller
+{
+    /**
+     * Show tests homepage
+     */
+    public function index(Request $request)
+    {
+        # code...
+    }
+
+    /**
+     * Show Raw DB Select page
+     */
+    public function dbSelect(Request $request)
+    {
+        # code...
+    }
+
+    /**
+     * Show Raw DB Select With Limit page
+     */
+    public function dbSelectWithLimit(Request $request)
+    {
+        # code...
+    }
+
+    /**
+     * Show PHP processed page
+     */
+    public function phpSearch(Request $request)
+    {
+        # code...
+    }
+
+    /**
+     * Show PHP processed page with stop page
+     */
+    public function phpSearchWithStop(Request $request)
+    {
+        # code...
+    }
+}
