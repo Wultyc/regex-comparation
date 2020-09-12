145e4914372d56707d9a961c9ff9b55b64123942
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2020-09-12 22:59:43 +0100

    Model base

--- app/Models/regex.php
+++ app/Models/regex.php
@@ -17,4 +17,36 @@ class regex extends Model
     protected $fillable = [
         'regex',
     ];
+
+    /**
+     * Make a raw SQL query evaluating the regex on DB side
+     */
+    public function selectAllWithWhere(string $target = null)
+    {
+        # code...
+    }
+
+    /**
+     * Make a raw SQL query evaluating the regex on DB side and limiting the results to 1
+     */
+    public function selectAllWithWhereAndLimit1(string $target = null)
+    {
+        # code...
+    }
+
+    /**
+     * Retrieve all records from db and evaluate using PHP resources
+     */
+    public function selectAllAndProcessLocally(string $target = null)
+    {
+        # code...
+    }
+
+    /**
+     * Retrieve all records from db, evaluating using PHP resources and stopping after finding the first match
+     */
+    public function selectAllAndProcessLocallyAndStopAfterFinding(string $target = null)
+    {
+        # code...
+    }
 }
